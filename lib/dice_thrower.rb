8e1cec280db64bc2b768932c4a2cb74f02718410
MaryJay787 <josephmaryam.mj@gmail.com>
2019-05-21 14:55:15 -0400

    Dice Roll Done

--- lib/dice_thrower.rb
+++ lib/dice_thrower.rb
@@ -1,1 +1,13 @@
-# Write your solution here
+require "pry"
+
+def throw_die
+  rand(1...6)
+end
+
+def generate_set(rolls)
+  die_array = Array.new
+  rolls.times do
+  die_array << throw_die
+  end
+  die_array
+end
